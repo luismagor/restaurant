d404d52d76a5d67f3b73852653c115e53d71f4af
Luis M. García <luismago@gmail.com>
2019-08-19 01:29:42 +0200

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: `Restaurant`,
+    description: `Restaurant website built with Gatsby`,
+    author: `@luismagor`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hello from Gatsby</h1>
+  </Layout>
+)
+
+export default IndexPage
