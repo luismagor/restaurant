db8625de90d67bd9506d1f470a0a8b92026ebe40
Luis M. García <luismago@gmail.com>
2019-08-19 21:14:50 +0200

    add banner button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
-import { HomeHeader, Banner } from "../utils"
+import { HomeHeader, Banner, BannerButton } from "../utils"
 import image from "../images/bcg/homeBcg.jpeg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomeHeader image={image}>
-      <Banner
-        title="eatery"
-        subtitle="55 main street — Santa Monica, CA"
-      ></Banner>
+      <Banner title="eatery" subtitle="55 main street — Santa Monica, CA">
+        <BannerButton style={{ margin: "2rem auto" }}>menu</BannerButton>
+      </Banner>
     </HomeHeader>
   </Layout>
 )
