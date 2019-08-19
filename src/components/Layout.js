bb0364b36505a80f9c036bfb53f874c3d464707c
Luis M. García <luismago@gmail.com>
2019-08-19 18:33:08 +0200

    add global styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
-import "./layout.css"
+import { createGlobalStyle } from "styled-components"
 import Navbar from "./globals/navbar"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyles />
       <Navbar />
       {children}
     </>
   )
 }
+
+const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Caveat|Open+Sans:400,400i,700,700i&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+  color: #262626;
+  background: #fff;
+}
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
