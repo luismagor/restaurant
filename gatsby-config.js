f69e6419856f9f9f9705785ab85a27983585830a
Luis M. García <luismago@gmail.com>
2019-08-25 22:53:36 +0200

    fix environment variables name

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        modulePath: `${__dirname}/src/cms/7c37421cbd40cdead2f96ab0bd729c2909772d0a.js`,
         manualInit: true,
       },
     },
--- src/cms/7c37421cbd40cdead2f96ab0bd729c2909772d0a.js
+++ src/cms/7c37421cbd40cdead2f96ab0bd729c2909772d0a.js
@@ -2,16 +2,14 @@ import CMS from "netlify-cms-app"
 import cloudinary from "netlify-cms-media-library-cloudinary"
 
 CMS.registerMediaLibrary(cloudinary)
-console.log(`${process.env.CLOUDINARY_USER}`)
-console.log(`${process.env.CLOUDINARY_API_KEY}`)
 
 CMS.init({
   config: {
     media_library: {
       name: "cloudinary",
       config: {
-        cloud_name: `${process.env.CLOUDINARY_USER}`,
-        api_key: `${process.env.CLOUDINARY_API_KEY}`,
+        cloud_name: `${process.env.GATSBY_CLOUDINARY_USER}`,
+        api_key: `${process.env.GATSBY_CLOUDINARY_API_KEY}`,
       },
     },
   },
