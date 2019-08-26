164f2df3c467000aa6defc8d2b0c6e290b2cefe3
Luis M. García <luismago@gmail.com>
2019-08-26 20:01:28 +0200

    cloudinary experiment

---
+++ src/cms/cms.js
@@ -0,0 +1,16 @@
+import CMS from "netlify-cms-app"
+import cloudinary from "netlify-cms-media-library-cloudinary"
+
+CMS.registerMediaLibrary(cloudinary)
+
+CMS.init({
+  config: {
+    media_library: {
+      name: "cloudinary",
+      config: {
+        cloud_name: processs.env.GATSBY_CLOUDINARY_CLOUD_NAME,
+        api_key: processs.env.GATSBY_CLOUDINARY_API_KEY,
+      },
+    },
+  },
+})
